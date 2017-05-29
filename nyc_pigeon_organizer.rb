d5e805e6fcfa1d8c64fd5fa974e2844472d6bfc1
Panagiotis Papadimitropoulos <ppapadim12@gmail.com>
2017-05-29 17:32:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |characteristic, specs|
+    specs.each do |specific_specs, names|
+      names.each do |each_name|
+        new_hash[each_name] = {}
+        end end end
+
+  new_hash.each do |name, empty_hash|
+    data.each do |attribute, details|
+      new_hash[name][attribute] = []
+      end end
+
+  data.each do |attributes, attr_hash|
+    attr_hash.each do |specific_char, names_arr|
+      names_arr.each do |name|
+        new_hash[name][attributes] << specific_char.to_s
+      end end end
+
+  new_hash
 end
